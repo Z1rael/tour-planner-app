fca3c08a73934aa3ffcf7d4918f38ea692decd2d
Felix Dilly <felix.dilly@gmail.com>
2026-03-02 20:02:00 +0100

    feat: java multi stage dockerfile done

--- backend/src/main/java/at/tourplanner/tour_planner/TourPlannerApplication.java
+++ backend/src/main/java/at/tourplanner/tour_planner/TourPlannerApplication.java
@@ -1,5 +1,7 @@
 package at.tourplanner.tour_planner;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TourPlannerApplication {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter name: ");
+		String name = scanner.next();
+		System.out.printf("\nHello, %s", name);
+
 		SpringApplication.run(TourPlannerApplication.class, args);
-		System.out.println	("Hello Worls!");
+
+		scanner.close();
 	}
 
 }

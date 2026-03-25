6c956e4a9ab0eac4a73acdeea0565a49eaad2c1b
Felix Dilly <felix.dilly@gmail.com>
2026-03-25 11:32:55 +0000

    feat: login form general structure done

--- frontend/src/app/components/login/login.ts
+++ frontend/src/app/components/login/login.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { LoginForm } from './models';
+import { email, form, FormField, required } from '@angular/forms/signals';
+import { debounce } from 'rxjs';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormField],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-  email: string = '';
-  password: string = '';
+  readonly loginModel = signal<LoginForm>({
+    email: '',
+    password: '',
+    rememberMe: false,
+  });
 
-  submit(): any {
-    if(this.email && this.password) {
-      console.log(`Login attempted with user: ${this.email}`)
-    }
+  readonly loginForm = form(this.loginModel, (schemaPath) => {
+    required(schemaPath.email, { message: 'Email is required' });
+    email(schemaPath.email, { message: 'Enter a valid email address' });
+
+    required(schemaPath.password, { message: 'Password is required' });
+  });
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+
+    const credentials = this.loginModel();
+
+    console.log('Logging in with: ', credentials);
   }
 }

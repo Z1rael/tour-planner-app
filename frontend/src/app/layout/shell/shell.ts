0953d2e174265bb923ff495f4a81809c8c076eea
Felix Dilly <felix.dilly@gmail.com>
2026-04-10 21:04:56 +0000

    feat: Text moves to top for small devices

--- frontend/src/app/layout/shell/shell.ts
+++ frontend/src/app/layout/shell/shell.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, inject, PLATFORM_ID, signal } from '@angular/core';
 import { Login } from '../../features/user/components/login/login';
 import { Register } from '../../features/user/components/register/register';
 import { TourList } from '../../features/tours/components/tour-list/tour-list';
@@ -8,6 +8,7 @@ import { ActivatedRoute } from '@angular/router';
 import { TourForm } from '../../features/tours/components/tour-form/tour-form';
 import { LogForm } from '../../features/tours/components/log-form/log-form';
 import { LogList } from '../../features/tours/components/log-list/log-list';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-shell',
@@ -17,11 +18,19 @@ import { LogList } from '../../features/tours/components/log-list/log-list';
   styleUrl: './shell.css',
 })
 export class Shell {
+  private readonly isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
+
+  readonly isNarrow = signal(this.isBrowser ? window.innerWidth <= 768 : false);
   currentView: string = '';
 
   constructor(private route: ActivatedRoute) {
     this.route.data.subscribe((data) => {
       this.currentView = data['view'];
     });
+  }
+
+  @HostListener('window:resize')
+  onResize(): void {
+    this.isNarrow.set(window.innerWidth <= 768);
   }
 }
